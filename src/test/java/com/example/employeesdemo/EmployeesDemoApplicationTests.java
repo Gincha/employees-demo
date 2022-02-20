17dceccabdc0731eecd8a22eab36a4b72c826dba
Gincha <bumbox1@inbox.lv>
2022-02-20 10:46:58 +0200

    initial commit

---
+++ src/test/java/com/example/employeesdemo/EmployeesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
