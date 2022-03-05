73df391f86a1732abddf18ba595389dbbd8ad007
Gincha <bumbox1@inbox.lv>
2022-03-05 14:08:03 +0200

    add and delete employees sample

--- src/main/java/com/example/employeesdemo/thymeleaf/controller/EmployeeMvcController.java
+++ src/main/java/com/example/employeesdemo/thymeleaf/controller/EmployeeMvcController.java
@@ -7,24 +7,47 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Data
 @Controller
-@RequestMapping("/employee")
-@Slf4j
+@RequestMapping("/employees")
 public class EmployeeMvcController {
 
-    private final EmployeeService employeeServiceImpl;
+    private final EmployeeService employeeService;
 
     @GetMapping()
     public String listEmployees(Model model) {
-        List<Employee> employeesList = employeeServiceImpl.getAllEmployees();
-        log.info("employeesList size: {}", employeesList.size());
+        List<Employee> employeesList = employeeService.getAllEmployees();
         model.addAttribute("employeesList", employeesList);
         return "employees-list";
+    }
+
+    @GetMapping("/form")
+    public String employeesForm(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees-form";
+    }
+
+    @GetMapping("/formForUpdate")
+    public String employeesFormForUpdate(@RequestParam("employeeId") Long empId, Model model){
+        Employee employee = employeeService.getEmployeeById(empId);
+        model.addAttribute("employee", employee);
+        return "/employees-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/employees";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") Long id){
+        employeeService.deleteEmployee(id);
+        return "redirect:/employees";
     }
 }
